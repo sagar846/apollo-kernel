0d8b3a543ed9a0954ee7a35c6b2510db795fdf13
Sagar Mohan <sagarmohan846@gmail.com>
2017-10-31 20:41:21 +0000

    initial commit

---
+++ cleanpkg.sh
@@ -0,0 +1,14 @@
+#
+# Cleans all old pkgs
+#
+echo '-------------------------------'
+echo '  Cleaning up older packages   '
+echo '-------------------------------'
+
+rm patch-*.*
+rm linux.install.pkg
+rm linux-apollo-*.pkg.tar.xz
+rm -rf pkg
+rm -rf src
+
+echo '------------Done---------------'
